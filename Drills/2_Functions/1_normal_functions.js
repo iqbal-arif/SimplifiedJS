08c0059d6e6ba1b2562048d566c33c96ee4a78cb
Arif Iqbal <arif_iqbal@yahoo.com>
2023-09-29 11:19:44 -0700

    modified:   Drills/2_Functions/1_normal_functions.js

--- Drills/2_Functions/1_normal_functions.js
+++ Drills/2_Functions/1_normal_functions.js
@@ -16,3 +16,11 @@ function sum(a,b){
     console.log(a+b)
 }
 sum(1,2)
+
+// 2.1 Write function that prints the name of a person.
+
+let person  = "Arif Iqbal"
+function personName(person){
+    console.log(person)
+}
+personName()
