5fade0f4397f45864adb23c2df446d72b4f4c12f
Arif Iqbal <arif_iqbal@yahoo.com>
2023-09-30 10:44:22 -0700

    modified:   Drills/2_Functions/2_passing_function_as_argument.js

--- Drills/2_Functions/2_passing_function_as_argument.js
+++ Drills/2_Functions/2_passing_function_as_argument.js
@@ -4,9 +4,12 @@ function printVariable(variable){
     console.log(variable)
 }
 
+console.log(printVariable)
 // Another function that takes printVariable as a function argument
 
 function func(x){
-    x("This string will initiate printVariable function")
+    x("This string initiated throgh printVariable function passed as variable")
 }
-console.log(printVariable)
+
+// Calling the func with other function as a variable
+func(printVariable)
