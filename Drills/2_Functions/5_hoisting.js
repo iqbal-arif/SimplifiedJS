d07e13d2adec77db38c6e2a71f0dc196c49a35ff
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-01 02:47:16 -0700

    modified:   Drills/2_Functions/5_hoisting.js

--- Drills/2_Functions/5_hoisting.js
+++ Drills/2_Functions/5_hoisting.js
@@ -1,3 +1,14 @@
-let sum = (a,b) => a + b
+// Hoisting works with regurlar function
+console.log(sum(3,4))
+
+function sum (a,b){
+    return a + b
+}
+
+
+// Hositing does not work with arrow function
+// Because arrow function is defined with variable
+// And variable are not hosited
 
 console.log(sum(1,2))
+let sum = (a,b) => a + b
