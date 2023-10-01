5542601cd31832d966009cd86b5043dc29c640b4
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-01 02:25:31 -0700

    modified:   Drills/2_Functions/3_arrow_functions.js

--- Drills/2_Functions/3_arrow_functions.js
+++ Drills/2_Functions/3_arrow_functions.js
@@ -18,3 +18,15 @@ console.log(printName("Arif"))
 // 2. Arrow Function one liner
  let printHi = (name) => `Hi ${name}`
  console.log(printHi("JavaScript"))
+
+//  3. Arrow function usage
+function displayNum(x, callback){
+    callback(x)
+}
+
+displayNum(10, function(number){
+    console.log(number)
+})
+
+// Rewriting above as an Arrow function
+displayNum(10, number => console.log(number))
