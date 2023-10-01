bb3d0f1eee890e697bba8b2353aab7e8e2fef18d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-01 02:33:38 -0700

    modified:   Drills/2_Functions/2_passing_function_as_argument.js

--- Drills/2_Functions/2_passing_function_as_argument.js
+++ Drills/2_Functions/2_passing_function_as_argument.js
@@ -77,3 +77,22 @@ function printName() {
   
   callback("hello " + name)
 }
+
+// Another Example of Passing function as an Argument
+
+function doStuff(a,b,name){
+    print(sum(a,b))
+    print(sayHi(name))
+}
+
+// This function just prints the results
+let print = (printStuff) => console.log(printStuff)
+
+// Sum funtion
+let sum = (a,b) => a + b
+
+// Greeting function
+let hi = (name) => `Hi ${name}`
+
+// Invokig a function
+doStuff(3,3, "JavaScript")
