90a13a6c314b082bdf04f8c19fc32969819748cb
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-03 02:05:09 -0700

    modified:   Drills/3_AdvancedVariables/2_coercion.js

--- Drills/3_AdvancedVariables/2_coercion.js
+++ Drills/3_AdvancedVariables/2_coercion.js
@@ -10,5 +10,9 @@ undefined
 // String to number called as Explicit Type Coercion
 
 let a  = "1"
-
+// parseInt for integer
 console.log(typeof parseInt(a))
+
+let b = "1.3"
+//  parseFloat for decimal number
+console.log(typeof parseFloat(a))
