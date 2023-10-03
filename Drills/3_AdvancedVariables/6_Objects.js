41d37934a7ba31f5e32b0fcebe31c140ba738c53
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-03 03:48:27 -0700

    new file:   Drills/3_AdvancedVariables/6_Objects.js

---
+++ Drills/3_AdvancedVariables/6_Objects.js
@@ -0,0 +1,30 @@
+let person = {
+    name:"Arif",
+    age:45,
+    favoriteNumber:3,
+    sayHi(){
+        console.log("Hi")
+    }
+}
+person.sayHi()
+console.log(person.name)
+
+const car ={
+    make: "Toyota",
+    model: "Avalon",
+    isUsed: true,
+    makeNoise(){
+        console.log("Vroom")
+    }
+}
+
+let employee = {
+    name: "Kyle",
+    hobbies: ["Weight Lifting", "Programing"],
+    address:{
+        street: "12334 street",
+        city: "CityHome"
+    }
+}
+
+console.log(employee.address)
