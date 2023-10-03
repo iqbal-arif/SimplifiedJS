6146caff00f59c813eb6174fde91b56dd4966fa2
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-03 03:17:57 -0700

    modified:   Drills/3_AdvancedVariables/5_Arrays.js

--- Drills/3_AdvancedVariables/5_Arrays.js
+++ Drills/3_AdvancedVariables/5_Arrays.js
@@ -2,3 +2,16 @@ const a = [1,2,3,4,5,6,7,8,9,10]
 
 console.log(a[0])
 console.log(a[9])
+
+a.push(11)
+
+console.log(a)
+a.push ("Hi")
+
+b = [["Hi", "Bye"],[1,2,3]]
+console.log(a[0][0])
+
+// Create an array with the first 5 letters of the alphabet and print c
+
+const alphabet = ["a","b","c","d","e"]
+console.log(alphabet[2])
