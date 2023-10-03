b5ba21c05263ed5b4c494a4072db727e01c29e0e
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-03 04:56:59 -0700

    modified:   Drills/3_AdvancedVariables/7_Reference_vs_Value.js

--- Drills/3_AdvancedVariables/7_Reference_vs_Value.js
+++ Drills/3_AdvancedVariables/7_Reference_vs_Value.js
@@ -3,10 +3,17 @@ let b = "Hi"
 let c = a // same reference
 c = c + 1 // same reference but adding hardcoded number to it
 let d = [1,2] 
+let f = [3,4,5] // new reference in memeory
 e = d // sharing same reference as d
 e.push(3) // both d and e will change
 
-
+let h = {name: "JavaScript"} 
+let i = h
+h.name = "Fullstack JavaScript"
 console.log("a = " + a)
 console.log("b = " + b)
 console.log("b = " + c)
+console.log("h = " + h) //displays as an object convert them with JSON.stringify()
+console.log("h = " + JSON.stringify(h)) //converting to string with JSON.stringify()
+console.log("i = " + i)
+console.log("i = " + JSON.stringify(i))
