55305de207c6cf69b51c489bafd04c672465180d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-03 02:06:06 -0700

    modified:   Drills/3_AdvancedVariables/2_coercion.js

--- Drills/3_AdvancedVariables/2_coercion.js
+++ Drills/3_AdvancedVariables/2_coercion.js
@@ -16,3 +16,9 @@ console.log(typeof parseInt(a))
 let b = "1.3"
 //  parseFloat for decimal number
 console.log(typeof parseFloat(a))
+
+// Number to String
+
+let c = 1.34
+console.log(typeof c.toString(c))
+
