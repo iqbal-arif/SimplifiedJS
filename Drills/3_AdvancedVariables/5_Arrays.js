3b8fd76c04b24a960c4950691519484b613d0c0b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-03 03:23:18 -0700

    modified:   Drills/3_AdvancedVariables/5_Arrays.js

--- Drills/3_AdvancedVariables/5_Arrays.js
+++ Drills/3_AdvancedVariables/5_Arrays.js
@@ -16,3 +16,13 @@ console.log(b[0][0])
 
 const alphabet = ["a","b","c","d","e"]
 console.log(alphabet[2])
+
+const x = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15]
+]
+// print out 4,8,11
+console.log(x[0][3])
+console.log(x[1][2])
+console.log(x[2][0])
