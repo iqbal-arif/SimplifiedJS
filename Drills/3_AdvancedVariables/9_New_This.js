6c87afa212020eda2d24ea999f55d9a328de5c8b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-04 01:48:42 -0700

    modified:   Drills/3_AdvancedVariables/9_New_This.js

--- Drills/3_AdvancedVariables/9_New_This.js
+++ Drills/3_AdvancedVariables/9_New_This.js
@@ -35,6 +35,6 @@ employee.printName()
 // Usecase
 
 const date = new Date()
-console.log(date.getDate)
-console.log(date.getMonth)
-console.log(date.getDay)
+console.log(date.getDate())
+console.log(date.getMonth())
+console.log(date.getDay())
