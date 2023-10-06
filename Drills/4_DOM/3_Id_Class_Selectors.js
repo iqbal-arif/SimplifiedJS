d525c21a9021c377a43a73903ce6fd8f0a36ab9e
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-06 11:56:36 -0700

    modified:   Drills/4_DOM/3_Id_Class_Selectors.js

--- Drills/4_DOM/3_Id_Class_Selectors.js
+++ Drills/4_DOM/3_Id_Class_Selectors.js
@@ -2,6 +2,14 @@
 const divWithID = (document.getElementById("div-id"))
 console.log(divWithID)
 divWithID.style.color = "red"
+
+
 // Get Element by Class
 
 const divWithClass = document.getElementsByClassName("div-class")
+
+// Converting div class listing to an Array
+
+const divWithClassArray = Array.from(divWithClass)
+
+divWithClass.forEach(div.style.color="green");
