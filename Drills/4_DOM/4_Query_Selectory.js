04d61248888787871cb5588dc47dba4418f3ce64
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-06 12:05:00 -0700

    modified:   Drills/4_DOM/4_Query_Selectory.js

---
+++ Drills/4_DOM/4_Query_Selectory.js
@@ -0,0 +1,8 @@
+// Query Selector select one element
+// Select any element that has data-test as an attribute
+const dataAttributeElement = document.querySelector('[data-test')
+
+console.log(dataAttributeElement)
+
+// Selecting the first element in class attribute
+const divWithClasses = document.querySelector('.div-class')
