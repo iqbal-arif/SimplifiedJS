a59e8304efc138835de768f3a09966c6a21dc7bb
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-06 00:35:43 -0700

    modified:   Drills/4_DOM/2_Window_Document.js

--- Drills/4_DOM/2_Window_Document.js
+++ Drills/4_DOM/2_Window_Document.js
@@ -6,3 +6,13 @@ window.alert("A window object")
 
 // Check window size
 window.addEventListener("resize", ()=> console.log("resized"))
+
+// Document Objects
+
+console.log(document)
+console.log(document.body)
+console.log(document.documentElement)
+
+const element = document.createElement("span")
+element.innerHTML = "Hello World"
+document.body.appendChild(element)
