58113bf7748631490e453d7f61e0556536e0b3a8
Arif Iqbal <arif_iqbal@yahoo.com>
2023-10-08 05:54:40 -0700

    modified:   Drills/4_DOM/4_Query_Selectory.js

--- Drills/4_DOM/4_Query_Selectory.js
+++ Drills/4_DOM/4_Query_Selectory.js
@@ -7,3 +7,7 @@ console.log(dataAttributeElement)
 // Selecting the first element in class attribute
 const divWithClasses = document.querySelector('.div-class')
 
+//  Selecting Array of class through QuerySelectorAll 
+const divWithClassesAll = document.querySelectorAll(".div-class")
+
+// Appl
