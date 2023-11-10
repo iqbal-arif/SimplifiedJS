a604f93db8b8d1e6fa4cd31fe27f77a8f912610e
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-10 10:56:20 -0800

    modified:   Drills/4_DOM/5_39_Event_Listeners.js

--- Drills/4_DOM/5_39_Event_Listeners.js
+++ Drills/4_DOM/5_39_Event_Listeners.js
@@ -17,5 +17,7 @@ btn.addEventListener("click", e => {console.log(e)})
 
 // Selecting the data in input field
 const input = document.querySelector('[data-input-text]')
-
+// EventListener "CHANGED" takes input but does not store any place just informs that input has changed
 input.addEventListener('change', ()=>{console.log("changed")})
+// EventListener "INPUT" takes input and takes every character
+input.addEventListener('input', e=>{console.log(e.target.value === '')})
