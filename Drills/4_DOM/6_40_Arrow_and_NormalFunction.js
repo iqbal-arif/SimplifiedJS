87aa1b81df30dfe27f4255fd61e55eaef80be855
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-12 10:40:53 -0800

    modified:   Drills/4_DOM/6_40_Arrow_and_NormalFunction.js

--- Drills/4_DOM/6_40_Arrow_and_NormalFunction.js
+++ Drills/4_DOM/6_40_Arrow_and_NormalFunction.js
@@ -1,6 +1,12 @@
 const button = document.querySelector('button');
-button.addEventListener('click', e => {})
+button.addEventListener('click', e => {
+    console.log('Arrow This')
+    console.log(this)
+})
 
 button.addEventListener('click', function(e){
-    
+    console.log('Function this')
+    console.log(this)
 })
+
+console.log(this === window) //true;this is a window object
