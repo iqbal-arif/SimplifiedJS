e57e89974492c97f16286bc68a57d861f65623b4
Arif Iqbal~ <arif_iqbal@yahoo.com>
2023-11-20 11:29:36 -0600

    modified:   Drills/4_DOM/8_42_DOM_Traversal.html modified:   Drills/4_DOM/8_42_DOM_Traversal.js

--- Drills/4_DOM/8_42_DOM_Traversal.js
+++ Drills/4_DOM/8_42_DOM_Traversal.js
@@ -8,4 +8,25 @@ grandParent.style.color = 'red';
 const parentOne = grandParent.children[0];
 parentOne.style.color = 'green';
 
-// Selecting and Modifying Parente
+// Selecting and Modifying Parent Two through Parent One
+parentTwo = parentOne.nextElementSibling;
+parentTwo.style.color = 'orange';
+
+// Selecting & Modifying Children from Parent
+const childOne = parentOne.children[0];
+const childTwo = childOne.nextElementSibling;
+
+childOne.style.color = 'pink';
+childTwo.style.color = 'olive';
+
+// Children Selector
+const kidOne = document.querySelector('#child-one');
+kidOne.style.color = 'cyan';
+
+// Selecting Parent & Modifying through child
+guardianOne = kidOne.parentElement;
+guardianOne.style.color = 'navy';
+
+// Selecting GrandParent through Parent
+grandGuardian = guardianOne.parentElement;
+grandGuardian.style.color = 'maroon';
