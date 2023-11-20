8ada2c0b62b38d6d99e4e4071ec75668821516e7
Arif Iqbal~ <arif_iqbal@yahoo.com>
2023-11-20 10:52:39 -0600

    modified:   Drills/4_DOM/7_41_Data_Attribute.html modified:   Drills/4_DOM/7_41_Data_Attribute.js

--- Drills/4_DOM/7_41_Data_Attribute.js
+++ Drills/4_DOM/7_41_Data_Attribute.js
@@ -1,4 +1,26 @@
-const test = document.querySelector('[data-test]')
+const test = document.querySelector('[data-test]');
 // Gives the data
-test.dataset
-//consoel.log(test.dataset)
+test.dataset;
+console.log(test.dataset);
+
+// Accessing each data set separately
+
+console.log(test.dataset.test); // 1234
+console.log(test.dataset.testTwo); // test-2
+
+// dataset can be used to set values
+
+test.dataset.test = 'A value injected through JS code';
+
+// Counting and Storing the Button Clicks through DataSet
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    // Counting & Storing Clicks of each Individual button
+    // Used pasrsInt to convert string to integer for counting
+    const currentClicks = parseInt(button.dataset.clicks);
+    button.dataset.clicks = currentClicks + 1;
+  });
+});
