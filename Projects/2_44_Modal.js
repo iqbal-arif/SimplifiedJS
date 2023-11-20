85d71325c162740d15d52fceab0437d97bb757d5
Arif Iqbal~ <arif_iqbal@yahoo.com>
2023-11-20 15:59:21 -0600

    modified:   Projects/2_44_Modal.html modified:   Projects/2_44_Modal.js

--- Projects/2_44_Modal.js
+++ Projects/2_44_Modal.js
@@ -2,8 +2,6 @@ const openModal = document.querySelector('#open-modal-btn');
 const overlayLayer = document.querySelector('#overlay');
 const modalLayer = document.querySelector('#modal');
 const closeModal = document.querySelector('#close-modal-btn');
-// const bodyLayer = document.getElementsByTagName('body');
-// console.log(bodyLayer);
 
 openModal.addEventListener('click', () => {
   overlayLayer.classList.add('open');
