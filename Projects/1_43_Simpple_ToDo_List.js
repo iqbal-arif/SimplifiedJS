8d94c3cad5372f1165521c95b235500f0638bf7f
Arif Iqbal~ <arif_iqbal@yahoo.com>
2023-11-20 12:33:57 -0600

    modified:   Projects/1_43_Simple_ToDo_List.html modified:   Projects/1_43_Simpple_ToDo_List.js

---
+++ Projects/1_43_Simpple_ToDo_List.js
@@ -0,0 +1,16 @@
+//1.  Select All elements
+const form = document.querySelector('#new-item-form');
+const list = document.querySelector('#list');
+const input = document.querySelector('#item-input');
+
+// 2. Submit a form Add a new Element on list
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //   console.log(input.value);
+
+  // 1. Create a new Item
+  // 2. Add that Item to the list
+  // 3. Clear Input
+  // 4. Setup event listener to delete Item when clicked
+});
+//
