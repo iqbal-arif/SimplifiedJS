4db673cc0ff73ad360fb087850c6b7a613ab91ce
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-24 23:51:34 -0600

    modified:   Drills/5_Control Flow/1_46_If.js

--- Drills/5_Control Flow/1_46_If.js
+++ Drills/5_Control Flow/1_46_If.js
@@ -8,3 +8,7 @@ if (userIsLoggedIn) {
 // Can use following conditions to be true to execute the code.
 //1. Boolean : true or false
 //2. Math operations: 2+1 === 3 ; this is true , so it runs the code
+//3. Number Zero : Using number '0' as condition; this is false, so it does'nt run the code.
+//4. Empty String: " "; this is also false, code does't run.
+// 5. Null :  is also false
+// 6. Undefined :  is also false
