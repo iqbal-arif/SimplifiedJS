879e67c401b4198c8908a6cc7bffcc180fc8332c
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-25 09:01:00 -0600

    new file:   Drills/5_Control Flow/4_49_For_Loop.js

---
+++ Drills/5_Control Flow/4_49_For_Loop.js
@@ -0,0 +1,12 @@
+for (let i = 0; i <= 10; i++) {
+  console.log('start ' + i);
+  if (i >= 5) break;
+  console.log('end ' + i);
+}
+
+// Teachers Solutions
+
+for (let i = 0; i <= 10; i++) {
+  if (i === 5) break;
+  console.log(i);
+}
