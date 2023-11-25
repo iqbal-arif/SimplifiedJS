854ab79e857130c7654daeed09d5c67b3529446e
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-24 23:48:04 -0600

    new file:   Drills/5_Control Flow/1_46_If.html modified:   Drills/5_Control Flow/1_46_If.js

---
+++ Drills/5_Control Flow/1_46_If.js
@@ -0,0 +1,10 @@
+const userIsLoggedIn = true;
+
+if (userIsLoggedIn) {
+  console.log('User Logged-in');
+} else {
+  console.log('Please login');
+}
+// Can use following conditions to be true to execute the code.
+//1. Boolean : true or false
+//2. Math operations: 2+1 === 3 ; this is true , so it runs the code
