69872dc3e609a2470e6a7935161e5468c98bf6bd
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-26 11:42:10 -0600

    modified:   Drills/5_Control Flow/6_51_Recursion.js

--- Drills/5_Control Flow/6_51_Recursion.js
+++ Drills/5_Control Flow/6_51_Recursion.js
@@ -1,1 +1,8 @@
 // Function that calls itself
+const item = ['sense.', 'make', 'all', 'will', 'This'];
+function reverseArray(item) {
+  //   for (let i = 0; i < item.length; i++) console.log(item[i]);
+  for (let i = item.length - 1; i >= 0; i--) console.log item[i];
+}
+
+console.log(reverseArray(['sense.', 'make', 'all', 'will', 'This']));
