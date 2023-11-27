d827af104a7eb1661e6c5def060bb18328e61f30
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-26 22:11:59 -0600

    modified:   Drills/5_Control Flow/6_51_Recursion.js

--- Drills/5_Control Flow/6_51_Recursion.js
+++ Drills/5_Control Flow/6_51_Recursion.js
@@ -1,8 +1,76 @@
-// Function that calls itself
-const item = ['sense.', 'make', 'all', 'will', 'This'];
-function reverseArray(item) {
-  //   for (let i = 0; i < item.length; i++) console.log(item[i]);
-  for (let i = item.length - 1; i >= 0; i--) console.log item[i];
+/*https://www.programiz.com/javascript/recursion
+
+Recursion is a process of calling itself. A function that calls itself is called a recursive function.
+
+*/
+// The syntax for recursive function is:
+
+function recurse() {
+  // function code
+  recurse();
+  // function code
 }
 
-console.log(reverseArray(['sense.', 'make', 'all', 'will', 'This']));
+recurse();
+/*
+Here, the recurse() function is a recursive function. It is calling itself inside the function.
+Working of recursion in JavaScript
+
+Once the condition is met, the function stops calling itself. This is called a base condition.
+
+To prevent infinite recursion, you can use if...else statement (or similar approach) where one branch makes the recursive call, and the other doesn't.
+
+So, it generally looks like this.
+*/
+function recurse() {
+  if (condition) {
+    recurse();
+  } else {
+    // stop calling recurse()
+  }
+}
+
+recurse();
+/*
+A simple example of a recursive function would be to count down the value to 1.
+*/
+
+// Example 1: Print Numbers
+
+// program to count down numbers to 1
+function countDown(number) {
+  // display the number
+  console.log(number);
+
+  // decrease the number value
+  const newNumber = number - 1;
+
+  // base case
+  if (newNumber > 0) {
+    countDown(newNumber);
+  }
+}
+
+countDown(4);
+/*
+Output
+
+4
+3
+2
+1
+
+In the above program, the user passes a number as an argument when calling a function.
+
+In each iteration, the number value is decreased by 1 and function countDown() is called until the number is positive. Here, newNumber > 0 is the base condition.
+
+This recursive call can be explained in the following steps:
+
+countDown(4) prints 4 and calls countDown(3)
+countDown(3) prints 3 and calls countDown(2)
+countDown(2) prints 2 and calls countDown(1)
+countDown(1) prints 1 and calls countDown(0)
+
+When the number reaches 0, the base condition is met, and the function is not called anymore.
+
+*/
